df0a7844394093dbcc9408880745611808fa1840
lavolpeofficial <info@la-volpe.it>
2026-09-13 13:47:42 +0200

    feat(provider-registry): wire engineering provider provisioning

--- apps/api/src/modules/provider-registry/provider-registry.module.ts
+++ apps/api/src/modules/provider-registry/provider-registry.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuditModule } from '../audit/audit.module';
+import { EngineeringProviderProvisioningService } from './engineering-provider-provisioning.service';
 import { ProviderRegistryService } from './provider-registry.service';
 import { ProviderRouterService } from './provider-router.service';
 
 @Module({
   imports: [AuditModule],
-  providers: [ProviderRegistryService, ProviderRouterService],
-  exports: [ProviderRegistryService, ProviderRouterService],
+  providers: [
+    ProviderRegistryService,
+    ProviderRouterService,
+    EngineeringProviderProvisioningService,
+  ],
+  exports: [
+    ProviderRegistryService,
+    ProviderRouterService,
+    EngineeringProviderProvisioningService,
+  ],
 })
 export class ProviderRegistryModule {}
